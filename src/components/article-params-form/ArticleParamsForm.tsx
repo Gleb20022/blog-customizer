e58e1cfac9a7ad2ad31706ac40e83ef382d4d414
Gleb Barov <glebabarov@yandex.ru>
2024-08-21 16:14:32 +0300

    added form panel toggle feature

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,23 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
-import styles from './ArticleParamsForm.module.scss';
+import { OnClick } from '../arrow-button/ArrowButton';
 
-export const ArticleParamsForm = () => {
+import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	onClick: OnClick;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const { isOpen, onClick } = props;
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={onClick} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
