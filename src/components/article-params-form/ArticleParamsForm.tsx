c9f7fd53874c45c60da1ab9675f95f124c088406
Gleb Barov <glebabarov@yandex.ru>
2024-08-22 23:20:21 +0300

    added form components, added padding for them, added form and app state, added click outside the sidebar feature

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,33 +1,114 @@
-import { useState } from 'react';
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
-import { Text } from '../text';
+import { useState, FormEvent, useRef } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+import { ArrowButton } from 'components/arrow-button';
+import { Button } from 'components/button';
+import { Text } from '../text';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+
+import {
+	ArticleStateType,
+	defaultArticleState,
+	OptionType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
+
+type ArticleParamsFormProps = {
+	setAppState: (state: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const { setAppState } = props;
+
+	const rootRef = useRef<HTMLDivElement>(null);
+
 	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] = useState(defaultArticleState);
 
 	const toggleForm = () => {
 		setIsOpen(!isOpen);
 	};
 
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setAppState(formState);
+		setIsOpen(false);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		setAppState(defaultArticleState);
+		setIsOpen(false);
+	};
+
+	const handleChange = (key: keyof ArticleStateType, value: OptionType) => {
+		setFormState({ ...formState, [key]: value });
+	};
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+		onChange: setIsOpen,
+		event: 'mousedown',
+	});
+
 	return (
-		<>
+		<div ref={rootRef}>
 			<ArrowButton isOpen={isOpen} onClick={toggleForm} />
 			<aside
 				className={clsx(styles.container, isOpen && styles.container_open)}>
-				<form className={styles.form}>
-					<Text as='h1' size={31} weight={800} uppercase dynamicLite>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text as='h1' size={31} weight={800} uppercase>
 						Задайте параметры
 					</Text>
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => handleChange('fontFamilyOption', option)}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name={'Размер Шрифта'}
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(option) => handleChange('fontSizeOption', option)}
+						title={'Размер шрифта'}></RadioGroup>
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) => handleChange('fontColor', option)}
+						title='Цвет шрифта'></Select>
+					<Separator></Separator>
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => handleChange('backgroundColor', option)}
+						title='Цвет фона'></Select>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => handleChange('contentWidth', option)}
+						title='Ширина контента'></Select>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
